c9e10e48389b390c1cd7e00b3ec11f25947ae5e1
Aarzoo Fathima <64885566+AarzooFathima@users.noreply.github.com>
2020-12-10 23:59:36 +0530

    Add files via upload

---
+++ Python 1.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment 1
+
+# In[9]:
+
+
+name = "abcdecf ghijk"
+
+
+# In[13]:
+
+
+name.capitalize()
+
+
+# In[11]:
+
+
+name.count("c")
+
+
+# In[12]:
+
+
+name.index("j")
+
+
+# In[14]:
+
+
+name.islower()
+
+
+# In[16]:
+
+
+name.title()
+
+
+# # Assignment 2
+
+# In[17]:
+
+
+lst = ["123",1.5,45,"xyz"]
+
+
+# In[18]:
+
+
+lst.copy()
+
+
+# In[19]:
+
+
+lst.count(45)
+
+
+# In[20]:
+
+
+lst.insert(0,"first")
+
+
+# In[21]:
+
+
+lst
+
+
+# In[22]:
+
+
+lst.append("last")
+
+
+# In[23]:
+
+
+lst
+
+
+# In[24]:
+
+
+lst.remove(1.5)
+
+
+# In[25]:
+
+
+lst
+
+
+# # Assignment 3
+
+# In[26]:
+
+
+dictionary = {"Key":"Value","key1":1,"key2":2}
+
+
+# In[27]:
+
+
+dictionary
+
+
+# In[28]:
+
+
+dictionary.copy()
+
+
+# In[29]:
+
+
+dictionary.items()
+
+
+# In[30]:
+
+
+dictionary.get("key2")
+
+
+# In[31]:
+
+
+dictionary["key2"]
+
+
+# In[33]:
+
+
+dictionary.pop("Key")
+
+
+# In[34]:
+
+
+dictionary
+
+
+# In[ ]:
+
+
+
+
